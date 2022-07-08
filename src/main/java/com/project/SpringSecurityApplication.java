18c0e105b0beb35d8d34b4de6835c604d030b8ee
Emerson Millard <emillard.code@gmail.com>
2022-07-07 22:59:11 -0700

    Made minor formatting changes.

--- src/main/java/com/project/SpringSecurityApplication.java
+++ src/main/java/com/project/SpringSecurityApplication.java
@@ -1,4 +1,4 @@
-package com.project.springsecurity;
+package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringSecurityApplication.class, args);
+
     }
 
 }
