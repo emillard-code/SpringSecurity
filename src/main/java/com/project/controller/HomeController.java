7fec8bfb28656d1ab60c3a80a0be1734b6827f2b
Emerson Millard <emillard.code@gmail.com>
2022-07-07 22:58:33 -0700

    Created a simple .jsp page with a controller class.

---
+++ src/main/java/com/project/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+
+        return "home.jsp";
+
+    }
+
+}
