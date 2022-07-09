1ae41bd331dbf9983dae79964e06b438455c4e67
Emerson Millard <emillard.code@gmail.com>
2022-07-08 21:44:25 -0700

    Added security configuration class.

---
+++ src/main/java/com/project/configuration/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.project.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+
+        List<UserDetails> users = new ArrayList<>();
+        users.add(User.withDefaultPasswordEncoder()
+                .username("Emillard")
+                .password("Password123")
+                .roles("USER")
+                .build());
+
+        return new InMemoryUserDetailsManager(users);
+
+    }
+
+}
