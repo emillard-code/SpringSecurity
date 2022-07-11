4d373069ccb4673ea34c807f2fbd4a7bb0c723c6
Emerson Millard <emillard.code@gmail.com>
2022-07-11 07:34:46 -0700

    Edited configuration class.

--- src/main/java/com/project/configuration/SecurityConfiguration.java
+++ src/main/java/com/project/configuration/SecurityConfiguration.java
@@ -1,12 +1,16 @@
 package com.project.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.ArrayList;
@@ -16,20 +20,35 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
+    public AuthenticationProvider authProvider() {
 
         // This method allows you to set up the credentials of the Spring Security login page.
-        List<UserDetails> users = new ArrayList<>();
-        users.add(User.withDefaultPasswordEncoder()
-                .username("Emillard")
-                .password("Password123")
-                .roles("USER")
-                .build());
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
 
-        return new InMemoryUserDetailsManager(users);
+        return provider;
 
     }
+
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//
+//        // This method allows you to set up the credentials of the Spring Security login page.
+//        List<UserDetails> users = new ArrayList<>();
+//        users.add(User.withDefaultPasswordEncoder()
+//                .username("Emillard")
+//                .password("Password123")
+//                .roles("USER")
+//                .build());
+//
+//        return new InMemoryUserDetailsManager(users);
+//
+//    }
 
 }
