58849efb4275eb7f3e39349679d1f76e20912f92
Emerson Millard <emillard.code@gmail.com>
2022-07-11 07:35:10 -0700

    Added security-related class.

---
+++ src/main/java/com/project/service/MyUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.project.service;
+
+import com.project.UserPrinciple;
+import com.project.model.User;
+import com.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = repository.findByUsername(username);
+
+        if (user==null) {
+
+            throw new UsernameNotFoundException("User 404");
+
+        }
+
+        return new UserPrinciple(user);
+
+    }
+
+}
